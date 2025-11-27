f5c485618674d46d4e90f24c365ec552497841ba
Yağmur Tetik <126063227+yagmurttk@users.noreply.github.com>
2025-11-27 17:24:18 +0300

    UI Tasarımı

---
+++ SignalRWebUI/ViewComponents/DefaultComponents/_DefaultBookATableComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SliderDtos;
+
+namespace SignalRWebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultBookATableComponentPartial: ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultBookATableComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Slider");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
