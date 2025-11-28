fceb1a139efd437eabdd403cb845b2b8cea312e2
Yağmur Tetik <126063227+yagmurttk@users.noreply.github.com>
2025-11-28 12:30:11 +0300

    Sepet İşlemleri

--- SignalRWebUI/Controllers/MenuController.cs
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BasketDtos;
 using SignalRWebUI.Dtos.ProductDtos;
 using System.Text;
 
@@ -26,36 +27,36 @@ namespace SignalRWebUI.Controllers
 
 
 
-        //        [HttpPost]
-        //        public async Task<IActionResult> AddBasket(int id, int menuTableId)
-        //        {
-        //            if (menuTableId == 0)
-        //            {
-        //                return BadRequest("MenuTableId 0 geliyor.");
-        //            }
+        [HttpPost]
+        public async Task<IActionResult> AddBasket(int id, int menuTableId)
+        {
+            if (menuTableId == 0)
+            {
+                return BadRequest("MenuTableId 0 geliyor.");
+            }
 
-        //            CreateBasketDto createBasketDto = new CreateBasketDto
-        //            {
-        //                ProductID = id,
-        //                MenuTableID = menuTableId // Gelen MenuTableID burada kullanılıyor
-        //            };
+            CreateBasketDto createBasketDto = new CreateBasketDto
+            {
+                ProductId = id,
+                MenuTableId = menuTableId // Gelen MenuTableID burada kullanılıyor
+            };
 
-        //            var client = _httpClientFactory.CreateClient();
-        //            var jsonData = JsonConvert.SerializeObject(createBasketDto);
-        //            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        //            var responseMessage = await client.PostAsync("https://localhost:7186/api/Basket", stringContent);
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createBasketDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7157/api/Basket", stringContent);
 
-        //            var client2 = _httpClientFactory.CreateClient();
-        //            //var jsonData2 = JsonConvert.SerializeObject(updateCategoryDto);
-        //            //StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
-        //            await client2.GetAsync("https://localhost:7186/api/MenuTables/ChangeMenuTableStatusToTrue?id=" + menuTableId);
+            var client2 = _httpClientFactory.CreateClient();
+            //var jsonData2 = JsonConvert.SerializeObject(updateCategoryDto);
+            //StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            await client2.GetAsync("https://localhost:7157/api/MenuTables/ChangeMenuTableStatusToTrue?id=" + menuTableId);
 
-        //            if (responseMessage.IsSuccessStatusCode)
-        //            {
-        //                return RedirectToAction("Index");
-        //            }
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
 
-        //            return Json(createBasketDto);
-        //        }
+            return Json(createBasketDto);
+        }
     }
 }
