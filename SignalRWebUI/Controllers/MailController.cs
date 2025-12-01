8dd4a7e56b86d855a5db3a097f0b62c69c5f56a3
Yağmur Tetik <126063227+yagmurttk@users.noreply.github.com>
2025-12-01 21:54:36 +0300

    Mail Gönderme İşlemleri

---
+++ SignalRWebUI/Controllers/MailController.cs
@@ -0,0 +1,43 @@
+﻿
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using SignalRWebUI.Dtos.MailDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CreateMailDto createMailDto)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon", "projemailikurs@gmail.com"); // Mailin kimden gönderileceği
+            mimeMessage.From.Add(mailboxAddressFrom); 
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", createMailDto.ReceiverMail); // Mail kime gidecek
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = createMailDto.Body; ;  // mailin içeriği
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = createMailDto.Subject; // mailin konusu
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("projemailikurs@gmail.com", "jyin qqhq ynrr vjqr");
+
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
